0cc01a8faa0e02ad788d70e5eaf566bd496a81a4
λhmad λjmi <ahmadajmi@outlook.com>
2014-07-10 15:41:54 +0200

    Ceaning video create controller Configs for user auth

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -14,21 +14,16 @@ class VideosController < ApplicationController
 
   def create
     # render plain: params[:video].inspect
-    # debugger
-    # https://www.youtube.com/watch?v=AzIQjwcJcVE
 
     @video = current_user.videos.build(video_params)
 
     @url = params[:video][:url].to_s
 
-    puts "==================>===>==>" + @url
     req_url = URI.parse('http://api.embed.ly/1/oembed?key=a7716080853c4d6d945624885ceb9ab9&url=' + params[:video][:url].to_s)
     req = Net::HTTP::Get.new(req_url.to_s)
     res = Net::HTTP.start(req_url.host, req_url.port) {|http|
         http.request(req)
     }
-
-    # puts JSON.parse(res.body)
 
     @json = JSON.parse(res.body)
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret
+  provider :twitter, Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret,
+  {
+    :secure_image_url => 'true',
+    :image_size => 'original',
+    :authorize_params => {
+      :force_login => 'true',
+      :lang => 'pt'
+    }
+  }
 end
