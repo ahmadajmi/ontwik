d8c61fa3c664ca5c53b3433f35c3236e0f02c130
λhmad λjmi <ahmadajmi@outlook.com>
2014-09-02 23:04:58 +0300

    Make the userlayout load for just the user show controller, created a method and view for profile edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
 
-  layout 'userlayout'
-
   def index
     @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    render :layout => 'userlayout'
+  end
+
+  def edit
     @user = User.find(params[:id])
   end
 
