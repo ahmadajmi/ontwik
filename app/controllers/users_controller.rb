9a36b104135d95525a42b441918619873bc0cbdb
λhmad λjmi <ahmadajmi@outlook.com>
2014-09-05 07:16:50 +0300

    Fix user permission for user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, except: [:show]
+  before_filter :correct_user?, :only => :edit
 
   def index
     @users = User.all
@@ -45,8 +46,8 @@ class UsersController < ApplicationController
   end
 
   private
-  def user_params
-    params.require(:user).permit(:name, :description)
-  end
+    def user_params
+      params.require(:user).permit(:name, :description)
+    end
 
 end
